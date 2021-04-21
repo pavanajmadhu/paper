6fb18015f8718dd966a68ba4bc85f01c20989b80
pavanajmadhu <81358710+pavanajmadhu@users.noreply.github.com>
2021-04-21 16:06:26 +0530

    Add files via upload

---
+++ ppaper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y){
+    
+    var options = {
+        'restitution':0.8,
+        'friction':3,
+        'density':1
+    }
+        this.box=Bodies.circle(x,y,10,options)
+       
+       this.image=loadImage("paper.png")
+        World.add(world,this.box);
+    }
+    display(){
+        var position=this.box.position;
+        
+    
+        imageMode(CENTER);
+    image(this.image,position.x,position.y,10,10);
+    
+       
+    }
+    
+    }
